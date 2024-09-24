9a4a9297edad458eff20f2547394e0c29c3fc7ae
VladTim <vdtimakov00@gmail.com>
2024-09-24 19:21:17 +0300

    Fix settings5

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Загрузка переменных окружения из .env файла
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
